6dd439abd8b3eb4ae9e93777a9925bcb2ceea3ed
VISHVESWRA <vishveswravichu@gmail.com>
2026-01-08 09:33:25 +0530

    fix: bug

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://your-frontend.onrender.com", // later
+      "https://project-pms.netlify.app/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
